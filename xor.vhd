c794d451d25ac52a64f886e53c88d108c1565a79
20163620 <36880723+20163620@users.noreply.github.com>
2018-02-27 10:59:08 +0200

    Add files via upload

--- xor.vhd
+++ xor.vhd
@@ -1,17 +1,16 @@
 -- File: xor.vhd
-git config --global user.name "Andzej Kuldos"
 library ieee;
 use ieee.std_logic_1164.all;
 
 -- the entity
 entity xor is
 port (
-i_a, i_b : in std_logic;
+i_c, i_b : in std_logic;
 o_f : out std_logic);
 end xor;
 
 -- the architecture
 architecture xor_arc of xor is
 begin
-o_f <= i_a xor i_b;
+o_f <= i_c xor i_b;
 end xor_arc;
